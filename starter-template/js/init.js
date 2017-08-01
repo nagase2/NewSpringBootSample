db84aaad0697ed8d1af2f8e8b1c3142f3ab6f765
yoshi-PC\yoshi <nagase2@gmail.com>
2017-08-02 00:42:29 +0900

    もう一つのHTMLテンプレートを追加

---
+++ starter-template/js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+  $('.tap-target').tapTarget('open');
+  $('.tap-target').tapTarget('close');
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
